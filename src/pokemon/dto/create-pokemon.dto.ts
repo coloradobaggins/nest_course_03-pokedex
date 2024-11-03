250b56fb9abf797d7ce149d4cbd59cf04f672871
colobaggins <bugio89@gmail.com>
2024-11-03 11:44:21 -0300

    validacion dto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,10 @@
-export class CreatePokemonDto {}
+import { IsNumber, IsString } from "class-validator";
+
+export class CreatePokemonDto {
+
+    @IsNumber()
+    no: number;
+
+    @IsString({ message: 'name debe ser un string' })
+    name: string;
+}
